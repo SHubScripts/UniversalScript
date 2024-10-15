8c34d2d8efe65cf1a9100a5d4e6c7f77a43c28f5
SHubScripts <146130422+SHubScripts@users.noreply.github.com>
2024-10-15 22:15:54 +0100

    UniversalScript.lua

--- UniversalScript.lua
+++ UniversalScript.lua
@@ -1,9 +1,9 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
-   Name = "Rayfield Example Window",
-   LoadingTitle = "Rayfield Interface Suite",
-   LoadingSubtitle = "by Sirius",
+   Name = "Cerberus - Universal",
+   LoadingTitle = "Cerberus - Universal",
+   LoadingSubtitle = "by trippy.11 & zuff445",
    ConfigurationSaving = {
       Enabled = true,
       FolderName = nil, -- Create a custom folder for your hub/game
